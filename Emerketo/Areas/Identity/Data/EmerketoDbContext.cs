1cd1f6525f4d82287d56f666db203458a3363f3a
Jonathan Mellergård John <jonathan.mellergard@gmail.com>
2023-05-25 22:29:41 +0200

    Add project files.

---
+++ Emerketo/Areas/Identity/Data/EmerketoDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Emerketo.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Emerketo.Areas.Identity.Data;
+
+public class EmerketoDbContext : IdentityDbContext<EmerketoUser>
+{
+    public EmerketoDbContext(DbContextOptions<EmerketoDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+        builder.ApplyConfiguration(new EmerketoUserEntityConfiguration());
+    }
+}
+
+public class EmerketoUserEntityConfiguration : IEntityTypeConfiguration<EmerketoUser>
+{
+	void IEntityTypeConfiguration<EmerketoUser>.Configure(EntityTypeBuilder<EmerketoUser> builder)
+	{
+        builder.Property(u => u.FirstName);
+		builder.Property(u => u.LastName);
+	}
+}
+
---
+++ Emerketo/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Emerketo.Areas.Identity.Data;
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("EmerketoDbContextConnection") ?? throw new InvalidOperationException("Connection string 'EmerketoDbContextConnection' not found.");
+
+builder.Services.AddDbContext<EmerketoDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<EmerketoUser>().AddEntityFrameworkStores<EmerketoDbContext>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
+
+app.Run();
